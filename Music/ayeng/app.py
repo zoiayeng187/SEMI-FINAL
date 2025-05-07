cb9ba05052293bf042fa91017b9e5d1b99e6e877
zoiayeng187 <invokerkael14@gmail.com>
2025-05-07 14:31:37 +0800

    third commit

--- Music/ayeng/app.py
+++ Music/ayeng/app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,4 +9,5 @@ def hello():
     return "Ayeng, Zoi Artchelo L. BSIT-||| B "
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT",5000))
+    app.run(host='0.0.0.0', port = port)
